fcb15e6ccfa8ecddf0dbd0d0b0d89e3238db53c3
lupmit <lexuanphuc.mit@gmail.com>
2023-11-05 23:49:24 +0700

    optimize performance

--- src/js/background.js
+++ src/js/background.js
@@ -1,16 +1,16 @@
 const ON_COLOR = '#8fce00';
 const OFF_COLOR = '#f44336';
 
-chrome.storage.local.set({ isSkipAds: true });
+chrome.storage.local.set({ skipAds: true });
 chrome.action.setBadgeText({ text: 'on' });
 chrome.action.setBadgeBackgroundColor({ color: ON_COLOR });
 
 const handleActionClick = () => {
-	chrome.storage.local.get(['isSkipAds'], ({ isSkipAds }) => {
-		const newStatus = !isSkipAds;
+	chrome.storage.local.get(['skipAds'], ({ skipAds }) => {
+		const newStatus = !skipAds;
 		chrome.action.setBadgeText({ text: newStatus ? 'on' : 'off' });
 		chrome.action.setBadgeBackgroundColor({ color: newStatus ? ON_COLOR : OFF_COLOR });
-		chrome.storage.local.set({ isSkipAds: newStatus });
+		chrome.storage.local.set({ skipAds: newStatus });
 	});
 };
 
--- src/js/content_script.js
+++ src/js/content_script.js
@@ -1,69 +1,68 @@
 // Init settings
 let counter = 0;
 let isSkipAds = true;
+const stylesToHideVideoAds = `.ad-container,.ad-div,.masthead-ad-control,.video-ads,.ytp-ad-progress-list,
+#ad_creative_3,#footer-ads,#masthead-ad,#player-ads,.ytd-mealbar-promo-renderer,
+#watch-channel-brand-div,#watch7-sidebar-ads,ytd-display-ad-renderer,
+ytd-compact-promoted-item-renderer,.html5-video-player.ad-showing video 
+{
+  display: none !important;
+}`;
 
-chrome.storage.local.get(['isSkipAds'], ({ isSkipAds: skipStatus }) => {
-	isSkipAds = skipStatus;
+chrome.storage.local.get(['skipAds'], ({ skipAds }) => {
+	isSkipAds = skipAds;
 });
 
 const onStorageChange = (changes) => {
-	if (changes['isSkipAds']) {
-		isSkipAds = changes['isSkipAds'].newValue;
+	if (changes['skipAds']) {
+		isSkipAds = changes['skipAds'].newValue;
 	}
 };
 chrome.storage.local.onChanged.addListener(onStorageChange);
 
 const hideVideoAds = () => {
-	if (!document.getElementById('yt-extension-style')) {
+	if (!document.getElementById('wetube-extension-style')) {
 		const headTag = document.head || document.getElementsByTagName('head')[0];
 		if (!headTag)
 			return void setTimeout(function () {
 				hideVideoAds();
 			}, 100);
 		const newStyleTag = document.createElement('style');
-		newStyleTag.id = 'yt-extension-style';
-		newStyleTag.appendChild(
-			document.createTextNode(
-				`.ad-container,.ad-div,.masthead-ad-control,.video-ads,.ytp-ad-progress-list,#ad_creative_3,#footer-ads,#masthead-ad,#player-ads,.ytd-mealbar-promo-renderer,#watch-channel-brand-div,#watch7-sidebar-ads,ytd-display-ad-renderer,ytd-compact-promoted-item-renderer,.html5-video-player.ad-showing video {display: none !important;}`
-			)
-		);
+		newStyleTag.id = 'wetube-extension-style';
+		newStyleTag.appendChild(document.createTextNode(stylesToHideVideoAds));
 		headTag.appendChild(newStyleTag);
 	}
 };
 
 const observer = new MutationObserver((mutations) => {
 	// Browser break time
-	if (counter++ % 2 === 0) return;
-
-	// Click close ads overlay
-	Array.from(document.querySelectorAll('.ytp-ad-overlay-close-button'))?.forEach((e) =>
-		e?.click()
-	);
-
-	// Click "Skip ads" button
-	const skippableAd = document.querySelectorAll('.ytp-ad-skip-button').length;
-	if (skippableAd) {
-		Array.from(document.querySelectorAll('.ytp-ad-skip-button'))?.forEach((e) =>
-			e?.click()
-		);
+	if (counter++ % 2 === 0) {
+		counter /= 2;
 		return;
 	}
 
 	// Skip ads
-	const adElement = document.querySelectorAll('.video-ads.ytp-ad-module')[0];
 	const videoTag = document.getElementsByTagName('video')[0];
-	const adActive = adElement && window.getComputedStyle(adElement).display !== 'none';
-	if (adActive) {
+	if (document.querySelector('.html5-video-player.ad-showing video')) {
 		videoTag.playbackRate = 16;
 		videoTag.muted = true;
 		videoTag.currentTime = 1e9;
 	}
+
+	// Click close ads overlay
+	document.querySelectorAll('.ytp-ad-overlay-close-button')?.forEach((e) => e?.click());
+
+	// Click "Skip ads" button
+	document.querySelectorAll('.ytp-ad-skip-button')?.forEach((e) => e?.click());
 });
 
 document.addEventListener('DOMContentLoaded', (event) => {
-	if (!isSkipAds) return;
+	if (!isSkipAds) {
+		document.getElementById('wetube-extension-style')?.remove();
+		return;
+	}
 
 	// Listen DOM is changed
-	observer.observe(document.body, { subtree: true, childList: true });
 	hideVideoAds();
+	observer.observe(document.body, { subtree: true, childList: true });
 });
